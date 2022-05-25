ff12bf0b0415885b16154e28277da2a0ea384bc9
thecaasantos <thecaasantos@gmail.com>
2022-05-25 04:14:34 -0300

    django polls s views

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from polls import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
